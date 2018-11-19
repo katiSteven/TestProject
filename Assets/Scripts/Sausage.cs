4e3f772d7743741a4b9196cd2839c5c6610e628f
Steven Emmanuel <ksteven925@gmail.com>
2018-11-19 21:00:51 +0530

    found something

--- Assets/Scripts/Sausage.cs
+++ Assets/Scripts/Sausage.cs
@@ -7,6 +7,7 @@ public class Sausage : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("You found the sausage");
+        print("You found something");
 	}
 	
 	// Update is called once per frame
