a67995a0c7aa7f1e0ede50b521f005d5e7435973
Steven Emmanuel <ksteven925@gmail.com>
2018-11-19 20:59:10 +0530

    you found the sausage

---
+++ Assets/Scripts/Sausage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sausage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("You found the sausage");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
